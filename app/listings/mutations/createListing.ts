3196239d8f35e3cbc9c43ebd7650d9c49fd018c3
Abu Uzayr <abu.uzayr@builtforfifty.com>
2022-01-08 18:58:02 +0800

    Update create listing schema

--- app/listings/mutations/createListing.ts
+++ app/listings/mutations/createListing.ts
@@ -2,13 +2,12 @@ import { resolver } from "blitz"
 import db from "db"
 import * as z from "zod"
 
-const CreateListing = z
-  .object({
-    name: z.string(),
-    category: z.string(),
-    tagline: z.string().max(80),
-  })
-  .nonstrict()
+const CreateListing = z.object({
+  name: z.string(),
+  category: z.string(),
+  tagline: z.string().max(80),
+  userId: z.number(),
+})
 
 export default resolver.pipe(resolver.zod(CreateListing), async (input) => {
   // TODO: in multi-tenant app, you must add validation to ensure correct tenant
