d6935820e7a12a3ad62f82b361a045bf183b8f5c
Abu Uzayr <abu.uzayr@builtforfifty.com>
2022-01-09 09:45:47 +0800

    Update login page styling

--- app/auth/pages/signup.tsx
+++ app/auth/pages/signup.tsx
@@ -1,14 +1,39 @@
 import { useRouter, BlitzPage, Routes } from "blitz"
 import Layout from "app/core/layouts/Layout"
 import { SignupForm } from "app/auth/components/SignupForm"
+import { Flex, Stack, Box, Avatar } from "@chakra-ui/react"
 
 const SignupPage: BlitzPage = () => {
   const router = useRouter()
 
   return (
-    <div>
-      <SignupForm onSuccess={() => router.push(Routes.Home())} />
-    </div>
+    <Flex
+      flexDirection="column"
+      width="100wh"
+      backgroundColor="white"
+      justifyContent="center"
+      alignItems="center"
+      style={{
+        height: "calc(100vh - 200px)",
+      }}
+    >
+      <Stack
+        flexDir="column"
+        mb="2"
+        justifyContent="center"
+        alignItems="center"
+        spacing={4}
+        p="2rem"
+        backgroundColor="whiteAlpha.900"
+        boxShadow="lg"
+        borderRadius="lg"
+      >
+        <Avatar bg="red.500" />
+        <Box minW={{ base: "90%", md: "468px" }}>
+          <SignupForm onSuccess={() => router.push(Routes.Home())} />
+        </Box>
+      </Stack>
+    </Flex>
   )
 }
 
