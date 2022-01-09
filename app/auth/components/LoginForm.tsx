6ef427d55d4ad01317af17c6ec012ab2116a09e4
Abu Uzayr <abu.uzayr@builtforfifty.com>
2022-01-09 09:45:15 +0800

    Update login form styling

--- app/auth/components/LoginForm.tsx
+++ app/auth/components/LoginForm.tsx
@@ -1,8 +1,9 @@
-import {AuthenticationError, Link, useMutation, Routes, PromiseReturnType} from "blitz"
-import {LabeledTextField} from "app/core/components/LabeledTextField"
-import {Form, FORM_ERROR} from "app/core/components/Form"
+import { AuthenticationError, Link as BLink, useMutation, Routes, PromiseReturnType } from "blitz"
+import { LabeledTextField } from "app/core/components/LabeledTextField"
+import { Form, FORM_ERROR } from "app/core/components/Form"
 import login from "app/auth/mutations/login"
-import {Login} from "app/auth/validations"
+import { Login } from "app/auth/validations"
+import { Center, HStack, Text, Link } from "@chakra-ui/react"
 
 type LoginFormProps = {
   onSuccess?: (user: PromiseReturnType<typeof login>) => void
@@ -13,19 +14,17 @@ export const LoginForm = (props: LoginFormProps) => {
 
   return (
     <div>
-      <h1>Login</h1>
-
       <Form
         submitText="Login"
         schema={Login}
-        initialValues={{email: "", password: ""}}
+        initialValues={{ email: "", password: "" }}
         onSubmit={async (values) => {
           try {
             const user = await loginMutation(values)
             props.onSuccess?.(user)
           } catch (error: any) {
             if (error instanceof AuthenticationError) {
-              return {[FORM_ERROR]: "Sorry, those credentials are invalid"}
+              return { [FORM_ERROR]: "Sorry, those credentials are invalid" }
             } else {
               return {
                 [FORM_ERROR]:
@@ -38,15 +37,22 @@ export const LoginForm = (props: LoginFormProps) => {
         <LabeledTextField name="email" label="Email" placeholder="Email" />
         <LabeledTextField name="password" label="Password" placeholder="Password" type="password" />
         <div>
-          <Link href={Routes.ForgotPasswordPage()}>
-            <a>Forgot your password?</a>
-          </Link>
+          <BLink href={Routes.ForgotPasswordPage()}>
+            <Link color="red.500" float="right" fontSize="sm">
+              Forgot your password?
+            </Link>
+          </BLink>
         </div>
       </Form>
 
-      <div style={{marginTop: "1rem"}}>
-        Or <Link href={Routes.SignupPage()}>Sign Up</Link>
-      </div>
+      <Center marginTop="1rem">
+        <HStack fontSize="sm" spacing={2}>
+          <Text>Or</Text>
+          <BLink href={Routes.SignupPage()} passHref>
+            <Link color="red.500">Sign Up</Link>
+          </BLink>
+        </HStack>
+      </Center>
     </div>
   )
 }
